3069da1128bd488d4f3958e996f5864820e52b73
Maxwell Da Silva <dayvson@gmail.com>
2013-04-13 17:23:56 -0400

    Init camera

---
+++ src/demo.js
@@ -0,0 +1,29 @@
+$(function(){
+    var self = this;
+    var webcam = document.getElementById("webcam");
+    var webcamCanvas = document.getElementById("webcam-canvas");
+    var lipCanvas = document.getElementById("lips-canvas");
+
+    var initDemo = function(){
+        webcam.play();
+        LipDetector.init(webcam, webcamCanvas, lipCanvas);
+        compatibility.requestAnimationFrame(LipDetector.tick);        
+    }
+    compatibility.getUserMedia({video: true}, 
+        function(stream) {
+            try {
+                webcam.src = compatibility.URL.createObjectURL(stream);
+            } catch (error) {
+                webcam.src = stream;
+            }
+            setTimeout(initDemo, 500);
+        }, 
+        function (error) {
+            console.log("ERROR");
+        }
+    );
+    $(window).unload(function() {
+        webcam.pause();
+        webcam.src = null;
+    });
+});
