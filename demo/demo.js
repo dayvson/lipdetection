b6de2349ac19284a82149b9378f1b829ff9935d6
Maxwell Da Silva <dayvson@gmail.com>
2013-04-17 22:30:24 -0400

    Added video samples

--- demo/demo.js
+++ demo/demo.js
@@ -2,59 +2,56 @@ $(function(){
     var webcam = document.getElementById("webcam");
     var webcamCanvas = document.getElementById("webcam-canvas");
     var lipCanvas = document.getElementById("lips-canvas");
-    var thumbCtx = document.getElementById("thumb-canvas").getContext('2d');
-    var loadImage = function(url){
+    var thumb = document.getElementById("thumb-canvas");
+    var webcamStream;
+    var loadVideo = function(url){
         LipDetector.stop();
-        LipDetector.useImage = true;
-        LipDetector.webcam = new Image();
-        LipDetector.webcam.onload = function() {
-            LipDetector.tick();
-        };
-        LipDetector.webcam.src = url;
-    };
-    var loadWebcam = function(){
-        LipDetector.stop();
-        LipDetector.useImage = false;
         LipDetector.webcam = webcam;
+        LipDetector.webcam.src = url;
+        LipDetector.webcam.load();
+        LipDetector.webcam.play();
         LipDetector.tick();
-    };
+    }
+
     $("#samples li").each(function(index, elem){
         $(elem).bind("click", function(){
             $("#samples li").css({"border":"1px solid white"});
             $(this).css({"border":"1px solid red"});
-            if($(this).hasClass("video")){
-                loadWebcam();
+            if($(this).hasClass("cam")){
+                initWebcam();
             }else{
-                loadImage(this.firstChild.src);
+                loadVideo(this.firstChild.getAttribute('data-video'));
             }
         });
     });
     
-    var drawWebcamThumb = function(){
-        compatibility.requestAnimationFrame(drawWebcamThumb);
-        thumbCtx.drawImage(webcam, 0, 0, 90, 90);
-    }
+    
     var initDemo = function(){
         webcam.play();
-        drawWebcamThumb();
+    
         LipDetector.init(webcam, webcamCanvas, lipCanvas);
         compatibility.requestAnimationFrame(function(){
             LipDetector.tick();
         });
     }
-    compatibility.getUserMedia({video: true}, 
-        function(stream) {
-            try {
-                webcam.src = compatibility.URL.createObjectURL(stream);
-            } catch (error) {
-                webcam.src = stream;
+    var initWebcam = function(){
+        compatibility.getUserMedia({video: true}, 
+            function(stream) {
+                try {
+                   webcam.src = compatibility.URL.createObjectURL(stream);
+                   thumb.src =  compatibility.URL.createObjectURL(stream);
+                } catch (error) {
+                    webcam.src = stream;
+                    thumb.src =  stream;
+                }
+                setTimeout(initDemo, 500);
+            }, 
+            function (error) {
+                console.log("ERROR");
             }
-            setTimeout(initDemo, 500);
-        }, 
-        function (error) {
-            console.log("ERROR");
-        }
-    );
+        );
+    }
+    initWebcam();
     $(window).unload(function() {
         webcam.pause();
         webcam.src = null;
