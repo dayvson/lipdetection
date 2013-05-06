37231ed7fc3c6008846cebd1a4c7efe63e4f0944
Carlo 'zED' Caputo <carlo.caputo@gmail.com>
2013-05-05 21:49:10 -0300

    libcontour2.js

--- src/lipdetector2.js
+++ src/lipdetector2.js
@@ -1,15 +1,3 @@
-var LipContour = {
-    find:function(imageData){
-        return [[314, 273.5],
-                [331.4, 260.5],
-                [343, 244.5],
-                [356, 260.5],
-                [364, 272.5],
-                [358, 287.5],
-                [342, 293.5],
-                [325, 290.5]];
-    }
-}
 var LipDetector = function(){};
 LipDetector.prototype = {
     init:function(webcam, webcamCanvas, lipCanvas, areaLips){
