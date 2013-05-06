21a0160a682799c245ff80f0d735d451609646f5
Carlo 'zED' Caputo <carlo.caputo@gmail.com>
2013-05-06 01:43:57 -0300

    better cleaning up of section

--- src/lipcontour2.js
+++ src/lipcontour2.js
@@ -1,9 +1,11 @@
 var LipContour = {
 	border: 4, // at least 1
 	max_mismatch: 999999999999,
+	clearance_range: 0.1,
 	getComponent:function(imageData, x,y, c) {
 		return imageData.data[(x + y*imageData.width)*4+c];
 	},
+	// TODO two modes upper/lower highlight
 	getMismatchComponent:function(imageData, x,y, c) {
 		var p11 = this.getComponent(imageData,x,y,c);
 		var p10 = this.getComponent(imageData,x,y-this.border,c);
@@ -28,11 +30,6 @@ var LipContour = {
 		// compute edges
 		var section = [];
 		var x = imageData.width/2;
-		for(var y0 = 0; y0 < this.border ; y0++) {
-			section[y0] = [this.max_mismatch, x,y0];
-			var y1 = imageData.height-1-y0;
-			section[y1] = [this.max_mismatch, x,y1];
-		}
 		for(var y = this.border; y < imageData.height-this.border ; y++) {
 			section[y] = [this.getMismatch(imageData,x,y), x,y];
 		}
@@ -41,14 +38,19 @@ var LipContour = {
 		var rank = section.slice(0).sort();
 		for(var i in rank) {
 			var y = rank[i][2];
-			if(section[y][0] == this.max_mismatch) continue;
-			for(var dy = 1; dy < this.border; dy++) {
-				section[y+dy][0] = section[y-dy][0] = this.max_mismatch; // invalidate point
+			if(section[y][0] == this.max_mismatch)
+				continue;
+			var clearance_range = imageData.height*this.clearance_range;
+			for(var dy = 1; dy < clearance_range; dy++) {
+				if(y+dy < imageData.height-this.border)
+					section[y+dy][0] = this.max_mismatch; // invalidate point
+				if(y-dy >= this.border)
+					section[y-dy][0] = this.max_mismatch; // invalidate point
 			}
 		}
 
 		// find top lines
-		var corner = section.sort().slice(0, limit);
+		var corner = section.slice(0).sort().slice(0, limit);
 		return corner;
 	},
     find:function(imageData){
